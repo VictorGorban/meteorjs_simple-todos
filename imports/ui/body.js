fba1585178a2662977d450439345fefaf0023653
victorgorban <victorgorban2@ya.ru>
2019-10-16 13:46:20 -0400

    done 'temporary UI state section. Got in the mind the reactive dictionary.'

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,18 +1,40 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js';
 
 import './task.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated() {
+	this.state = new ReactiveDict();
+	this.state.set('hideCompleted', true);
+});
+
 Template.body.helpers({
 	tasks() {
+		const instance = Template.instance();
+		if (instance.state.get('hideCompleted')) {
+			// If hide completed is checked, filter tasks
+			return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+		}
+		// Otherwise, return all of the tasks
 		return Tasks.find({}, {sort: {createdAt: -1} });
+	},
+	incompleteCount() {
+		return Tasks.find({ checked: { $ne: true } }).count();
+	},
+	isCompletedTasksHidden() {
+		const instance = Template.instance();
+		return instance.state.get('hideCompleted');
 	},
 });
 
 
 Template.body.events({
+	'change .hide-completed input'(event, instance) {
+		instance.state.set('hideCompleted', event.target.checked);
+	},
 	'submit .new-task'(event) {
 		// Prevent default browser form submit
 		event.preventDefault();
